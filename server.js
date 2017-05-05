babc3d129d16f1adadac5eb6c2c5061868ecf357
Harsh Goel <harsh_goel@ymail.com>
2017-05-05 15:43:53 -0700

    Adding the react UI

--- server.js
+++ server.js
@@ -5,25 +5,22 @@ var express = require('express');
 var routes = require('./routes/index');
 var http = require('http');
 var bodyParser = require('body-parser');
-
+var path = require('path');                 //new
 var projects = require('./routes/project');
-var register = require('./routes/register');
+
 
 var app = express();
 app.set('port',3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'src')));//new
 
 app.get('/',routes.index);
 app.set('views',__dirname + '/views');
 app.set('view engine','ejs');
 
-app.get('/projects', projects.getAllProjects);
-app.post('/projects', projects.addProject);
-
-app.post('/registerUser',register.registerUser);
-app.post('/login',register.login);
-app.get('/login', register.getAllUsers);
+app.get('/projects', projects.getAllProjects)
+app.post('/projects', projects.addProject)
 
 module.exports = app;
 
